27ffd1db2f8420848fd4f1bfea7cba1d4fd4d63a
np170577-png <np170577@gmail.com>
2026-04-19 19:21:29 +0300

    First Class

--- src/main/java/experiments/First.java
+++ src/main/java/experiments/First.java
@@ -1,4 +1,37 @@
 package experiments;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class First {
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+
+    public void firstTest(){
+        driver.get("https://demoqa.com/");
+        pause(5000);
+        driver.manage().window().maximize();
+        pause(2000);
+        driver.get("https://demoqa.com/elements");
+        pause(5000);
+        driver.navigate().back();
+        pause(4000);
+        driver.navigate().forward();
+        pause(4000);
+        driver.navigate().refresh();
+        pause(2000);
+        //driver.close();
+        driver.quit();
+    }
+
+    public void pause(int time){
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
